b81aa0f3104f0261e4189a18a8335a51e5b07f3b
Amiitesh TSP <tspamiitesh@gmail.com>
2024-11-28 06:41:36 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,9 @@
+/*
+I will get to the point as to explaining each and every keywords and words used in the program in the upcoming days.
+*/
+
+#include <stdio.h> //stdio.h is the library that includes program for input/output and declaring macros and etc.
+int main(){
+  printf("Hello World\n");
+  return 0;
+}
